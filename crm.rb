51b51df9cd351f0159fa5248aed51afbad8839af
Billy Davidson <billy.davids52@gmail.com>
2018-08-03 16:05:53 -0400

    contacts done

--- crm.rb
+++ crm.rb
@@ -5,3 +5,22 @@ require_relative 'contact_new'
 get '/' do
   erb :index
 end
+
+get '/contacts' do
+  @contacts = Contact.all     # all is a method that comes from active record base.
+  erb :contacts
+end
+
+get '/about' do
+  erb :about
+end
+
+
+
+
+
+
+
+after do
+  ActiveRecord::Base.connection.close
+end
